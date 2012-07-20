873549431fd0fc963bc5dd1ce3f05e9357809c57
Jean-Pierre Simard <jp@jpsim.com>
2012-07-19 23:28:03 -0400

    Refactored for better separation

--- Classes/MixTwoAudioFilesDemoAppDelegate.m
+++ Classes/MixTwoAudioFilesDemoAppDelegate.m
@@ -23,32 +23,48 @@
     [window addSubview:viewController.view];
     [window makeKeyAndVisible];
   
-  NSString *resName1 = @"toms.caf";
-//  NSString *resName2 = @"tk.caf";
+  NSArray *files = [self getFiles];
+  NSArray *times = [self getTimes];
+  NSString *mixURL = [self getMixURL];
   
-	NSString *resPath1 = [[NSBundle mainBundle] pathForResource:resName1 ofType:nil];
-//  NSString *resPath2 = [[NSBundle mainBundle] pathForResource:resName2 ofType:nil];
+	OSStatus status = [PCMMixer mixFiles:files atTimes:times toMixfile:mixURL];
   
-  //	NSString *tmpDir = NSTemporaryDirectory();
+  [self playMix:mixURL withStatus:status];
+}
+
+- (void)dealloc {
+    [viewController release];
+    [window release];
+    [avAudio release];
+    [super dealloc];
+}
+
+- (NSArray*)getFiles {
+  NSString *inFile = [[NSBundle mainBundle] pathForResource:@"toms.caf" ofType:nil];
+  return [NSArray arrayWithObjects:inFile,inFile,inFile,inFile, nil];
+}
+
+- (NSArray *)getTimes {
+  //  First item must be at time 0. All other sounds must be relative to this first sound.
+  return [NSArray arrayWithObjects:[NSNumber numberWithInt:0],[NSNumber numberWithInt:1],[NSNumber numberWithInt:2],[NSNumber numberWithInt:3], nil];
+}
+
+- (NSString*)getMixURL {
   NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-  NSString *docDir = ([paths count] > 0) ? [paths objectAtIndex:0] : nil;
-	NSString *tmpFilename = @"Mix.caf";
-	NSString *tmpPath = [docDir stringByAppendingPathComponent:tmpFilename];
-  
-	OSStatus status;
-  
-	status = [PCMMixer mixFiles:[NSArray arrayWithObjects:resPath1,resPath1,resPath1,resPath1, nil] atTimes:[NSArray arrayWithObjects:[NSNumber numberWithInt:0],[NSNumber numberWithInt:1],[NSNumber numberWithInt:2],[NSNumber numberWithInt:3], nil] toMixfile:tmpPath];
-  
-	if (status == OSSTATUS_MIX_WOULD_CLIP) {
+  return [[paths objectAtIndex:0] stringByAppendingPathComponent:@"Mix.caf"];
+}
+
+- (void)playMix:(NSString*)mixURL withStatus:(OSStatus)status {
+  if (status == OSSTATUS_MIX_WOULD_CLIP) {
 		[viewController.view setBackgroundColor:[UIColor redColor]];
 	} else {
 		[viewController.view setBackgroundColor:[UIColor greenColor]];
     
-		NSURL *url = [NSURL fileURLWithPath:tmpPath];
+		NSURL *url = [NSURL fileURLWithPath:mixURL];
     
 		NSData *urlData = [NSData dataWithContentsOfURL:url];
     
-		NSLog(@"wrote mix file of size %d : %@", [urlData length], tmpPath);
+		NSLog(@"wrote mix file of size %d : %@", [urlData length], mixURL);
     
 		AVAudioPlayer *avAudioObj = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
     [avAudioObj autorelease];
@@ -57,14 +73,6 @@
 		[avAudioObj prepareToPlay];
 		[avAudioObj play];
 	}
-
-}
-
-- (void)dealloc {
-    [viewController release];
-    [window release];
-    [avAudio release];
-    [super dealloc];
 }
 
 @end
